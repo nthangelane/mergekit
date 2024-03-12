nthangelane/mergekit

commit 3c77dea70e9b86cdc3f84a3621d3827cf8c57e56

Computation Graph Overhaul (Again) (#127)
Refactor the internal computational graph to make writing new tasks easier.

This time for real.

mergekit/merge_methods/passthrough.py
@@ -13,29 +13,44 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see http://www.gnu.org/licenses/.
 
-from typing import Dict
+from typing import Any, Dict, List, Optional
 
 import torch
+from torch._tensor import Tensor
 
-from mergekit.config import ConfigReader
-from mergekit.graph import TensorReference
-from mergekit.merge_methods.base import MergeMethod
+from mergekit.common import ImmutableMap, ModelReference
+from mergekit.graph import Task
+from mergekit.io.tasks import GatherTensors
+from mergekit.merge_methods.base import ConfigParameterDef, MergeMethod
+
+
+class PassthroughMergeTask(Task[torch.Tensor]):
+    gather_tensors: GatherTensors
+    scale: Optional[float] = None
+
+    def arguments(self) -> Dict[str, Task]:
+        return {"tensors": self.gather_tensors}
+
+    def execute(self, tensors: Dict[ModelReference, torch.Tensor]) -> Tensor:
+        if len(tensors) != 1:
+            raise RuntimeError("Passthrough merge expects exactly one tensor")
+
+        res = list(tensors.values())[0]
+        if self.scale is not None:
+            res *= self.scale
+
+        return res
 
 
 class PassthroughMerge(MergeMethod):
-    def __call__(
+    def parameters(self) -> List[ConfigParameterDef]:
+        return [ConfigParameterDef(name="scale", required=False, default_value=None)]
+
+    def make_task(
         self,
-        parameter_name: str,
-        input_tensors: Dict[TensorReference, torch.Tensor],
-        config: ConfigReader,
-        **_kwargs,
-    ) -> torch.Tensor:
-        if len(input_tensors) != 1:
-            raise RuntimeError("Passthrough merge expects exactly one tensor")
-
-        res = list(input_tensors.values())[0]
-        scale = config.parameter("scale")
-        if scale is not None:
-            res *= scale
-
-        return res
+        *,
+        tensors: GatherTensors,
+        parameters: ImmutableMap[str, Any],
+        **kwargs,
+    ) -> Task:
+        return PassthroughMergeTask(gather_tensors=tensors, scale=parameters["scale"])

.github/workflows/pre-commit.yml
@@ -16,6 +16,7 @@ jobs:
       - uses: pre-commit/action@v3.0.0
 
   pytest:
+    if: github.ref == 'refs/heads/master' || github.ref == 'refs/heads/dev' || github.event_name == 'pull_request'
     name: PyTest
     needs: [pre-commit]
     runs-on: ubuntu-latest

mergekit/architecture.py
@@ -81,6 +81,15 @@ class StaticTensorNames(ArchitectureInfo, BaseModel, frozen=True):
     def num_layers(self, config: PretrainedConfig) -> int:
         return getattr(config, self.num_layers_config_key())
 
+    def all_weights(self, config: PretrainedConfig) -> List[str]:
+        num_layers = self.num_layers(config)
+        tensor_names = list(self.pre_weights())
+        for layer_idx in range(num_layers):
+            for f in self.layer_weight_formats():
+                tensor_names.append(f.format(idx=layer_idx))
+        tensor_names.extend(self.post_weights())
+        return tensor_names
+
 
 LLAMA_INFO = StaticTensorNames(
     name="LlamaForCausalLM",

mergekit/card.py
@@ -75,11 +75,11 @@ def extract_hf_paths(models: List[ModelReference]) -> Sequence[str]:
         models: A list of ModelReference objects.
     """
     for model in models:
-        if is_hf(model.path):
-            yield model.path
+        if is_hf(model.model.path):
+            yield model.model.path
 
-        if model.lora_path and is_hf(model.lora_path):
-            yield model.lora_path
+        if model.lora and is_hf(model.lora.path):
+            yield model.lora.path
 
 
 def method_md(merge_method: str) -> str:
@@ -122,9 +122,9 @@ def modelref_md(model: ModelReference) -> str:
     Returns:
         A markdown formatted string describing the model reference.
     """
-    text = maybe_link_hf(model.path)
-    if model.lora_path:
-        text += " + " + maybe_link_hf(model.lora_path)
+    text = maybe_link_hf(model.model.path)
+    if model.lora:
+        text += " + " + maybe_link_hf(model.lora.path)
     return text
 
 
@@ -147,7 +147,7 @@ def generate_card(
     hf_bases = list(extract_hf_paths(config.referenced_models()))
     tags = ["mergekit", "merge"]
 
-    actual_base = ModelReference.parse(config.base_model) if config.base_model else None
+    actual_base = config.base_model
     if config.merge_method == "slerp":
         # curse my past self
         actual_base = None

mergekit/common.py
@@ -13,20 +13,65 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see http://www.gnu.org/licenses/.
 
+import binascii
 import logging
 import os
 import os.path
-from typing import List, Optional, Union
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Generic,
+    Iterator,
+    List,
+    Mapping,
+    Optional,
+    Tuple,
+    Union,
+    get_args,
+)
 
 import huggingface_hub
-import numpy as np
+import immutables
 import peft
 import torch
 import transformers
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
+from pydantic_core import core_schema
 from transformers import AutoConfig, PretrainedConfig
+from typing_extensions import TypeVar
 
 from mergekit.io import ShardedTensorIndex
+
+
+class ModelPath(BaseModel, frozen=True):
+    path: str
+    revision: Optional[str] = None
+
+    @model_validator(mode="before")
+    def validate_string(cls, value):
+        if isinstance(value, str):
+            at_ct = value.count("@")
+            if at_ct > 1:
+                raise RuntimeError(f"Invalid model path - multiple @: {value}")
+            elif at_ct == 1:
+                path, rev = value.split("@")
+                return {"path": path, "revision": rev}
+            else:
+                return {"path": value}
+        return value
+
+    def __str__(self):
+        if self.revision:
+            return f"{self.path}@{self.revision}"
+        return self.path
+
+    def _unique_id(self):
+        return (
+            os.path.basename(self.path)
+            + "_"
+            + str(binascii.crc32(self.__str__().encode()))
+        )
 
 
 class ModelReference(BaseModel, frozen=True):
@@ -34,14 +79,14 @@ class ModelReference(BaseModel, frozen=True):
 
     Can be a hf hub path (username/repo), or local. Optionally includes a LoRA."""
 
-    path: str
-    lora_path: Optional[str] = None
+    model: ModelPath
+    lora: Optional[ModelPath] = None
 
     def merged(
         self, cache_dir: Optional[str] = None, trust_remote_code: bool = False
     ) -> "ModelReference":
         """Merge the LoRA if applicable and return a reference to the result."""
-        if not self.lora_path:
+        if not self.lora:
             return self
 
         if not cache_dir:
@@ -49,41 +94,46 @@ class ModelReference(BaseModel, frozen=True):
 
         out_path = os.path.join(
             cache_dir,
-            os.path.basename(self.path) + "_" + os.path.basename(self.lora_path),
+            self.model._unique_id() + "_" + self.lora._unique_id(),
         )
 
         if not os.path.exists(out_path):
             os.makedirs(out_path, exist_ok=True)
-            logging.info(f"Loading {self.path} for merge...")
+            logging.info(f"Loading {self.model} for merge...")
             model = transformers.AutoModelForCausalLM.from_pretrained(
-                self.path,
+                self.model.path,
+                revision=self.model.revision,
                 torch_dtype=torch.float16,
                 low_cpu_mem_usage=True,
                 trust_remote_code=trust_remote_code,
             )
             model = peft.PeftModel.from_pretrained(
-                model, self.lora_path, is_trainable=False
+                model, self.lora.path, revision=self.lora.revision, is_trainable=False
             )
-            logging.info(f"Merging {self.lora_path} into {self.path}")
+            logging.info(f"Merging {self.lora} into {self.model}")
             model = model.merge_and_unload()
             model.save_pretrained(out_path, safe_serialization=True)
             del model
 
-        return ModelReference(path=out_path)
+        return ModelReference(model=out_path)
 
     def config(self, trust_remote_code: bool = False) -> PretrainedConfig:
         return AutoConfig.from_pretrained(
-            self.path, trust_remote_code=trust_remote_code
+            self.model.path,
+            revision=self.model.revision,
+            trust_remote_code=trust_remote_code,
         )
 
     def tensor_index(self, cache_dir: Optional[str] = None) -> ShardedTensorIndex:
-        assert self.lora_path is None
+        assert self.lora is None
 
-        path = self.path
+        path = self.model.path
         if not os.path.exists(path):
             has_safetensors = any(
                 fn.lower().endswith(".safetensors")
-                for fn in huggingface_hub.list_repo_files(path, repo_type="model")
+                for fn in huggingface_hub.list_repo_files(
+                    path, repo_type="model", revision=self.model.revision
+                )
             )
             patterns = ["tokenizer.model", "*.json"]
             if has_safetensors:
@@ -92,26 +142,34 @@ class ModelReference(BaseModel, frozen=True):
                 patterns.append("*.bin")
 
             path = huggingface_hub.snapshot_download(
-                path, cache_dir=cache_dir, allow_patterns=patterns
+                path,
+                revision=self.model.revision,
+                cache_dir=cache_dir,
+                allow_patterns=patterns,
             )
 
         return ShardedTensorIndex.from_disk(path)
 
+    @model_validator(mode="before")
+    def validate_string(cls, value):
+        if isinstance(value, str):
+            chunks = value.split("+")
+            if len(chunks) == 1:
+                return {"model": value}
+            elif len(chunks) == 2:
+                return {"model": chunks[0], "lora": chunks[1]}
+            raise RuntimeError(f"Can't parse {value}")
+        return value
+
     @classmethod
     def parse(cls, value: str) -> "ModelReference":
         """Parse a ModelReference. Format: '<MODEL_PATH>(+<LORA_PATH>)?'"""
-
-        chunks = value.split("+")
-        if len(chunks) == 1:
-            return ModelReference(path=value)
-        elif len(chunks) == 2:
-            return ModelReference(path=chunks[0], lora_path=chunks[1])
-        raise ValueError(f"Can't parse {value}")
+        return ModelReference.model_validate(value)
 
     def __str__(self) -> str:
-        if self.lora_path:
-            return f"{self.path}+{self.lora_path}"
-        return self.path
+        if self.lora:
+            return f"{str(self.model)}+{str(self.lora)}"
+        return str(self.model)
 
 
 def dtype_from_name(name: Optional[str]) -> torch.dtype:
@@ -151,19 +209,6 @@ def take_common_submatrix(tensors: List[torch.Tensor]) -> bool:
     return False
 
 
-def gradient_weights(gradient: List[float], num_samples: int) -> List[float]:
-    assert len(gradient) > 1, "Need at least two values to define gradient"
-
-    samples_per_weight = num_samples // (len(gradient) - 1)
-
-    res = []
-    for y0, y1 in zip(gradient[:-1], gradient[1:]):
-        res.extend(np.linspace(y0, y1, num=samples_per_weight))
-    while len(res) < num_samples:
-        res.append(gradient[-1])
-    return res
-
-
 def parse_kmb(value: Union[str, int]) -> int:
     if isinstance(value, int):
         return value
@@ -177,3 +222,49 @@ def parse_kmb(value: Union[str, int]) -> int:
         return int(value[:-1]) * 1000 * 1000 * 1000
     else:
         raise ValueError(value)
+
+
+T_K = TypeVar("T_K")
+T_V = TypeVar("T_V")
+
+
+class ImmutableMap(Generic[T_K, T_V]):
+    data: immutables.Map[T_K, T_V]
+
+    def __init__(self, data: Mapping[T_K, T_V]):
+        self.data = data
+
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls, source: Any, handler: Callable[[Any], core_schema.CoreSchema]
+    ) -> core_schema.CoreSchema:
+        instance_schema = core_schema.is_instance_schema(cls)
+
+        args = get_args(source)
+        if args:
+            dict_schema = handler(Dict[args[0], args[1]])
+        else:
+            dict_schema = handler(Dict)
+
+        non_instance_schema = core_schema.with_info_after_validator_function(
+            lambda value, _info: immutables.Map(value), dict_schema
+        )
+        return core_schema.union_schema([instance_schema, non_instance_schema])
+
+    def __iter__(self):
+        return self.data.__iter__()
+
+    def __getitem__(self, key: T_K) -> T_V:
+        return self.data[key]
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def keys(self) -> Iterator[T_K]:
+        return self.data.keys()
+
+    def items(self) -> Iterator[Tuple[T_K, T_V]]:
+        return self.data.items()
+
+    def values(self) -> Iterator[T_V]:
+        return self.data.values()

mergekit/config.py
@@ -54,7 +54,7 @@ def evaluate_setting(
                 if (
                     (cond.filter is None)
                     or (cond.filter == "*")
-                    or cond.filter in tensor_name
+                    or (tensor_name and cond.filter in tensor_name)
                 ):
                     res = evaluate_setting(tensor_name, cond.value, t)
                     return res
@@ -64,19 +64,19 @@ def evaluate_setting(
 
 
 class InputSliceDefinition(BaseModel):
-    model: str
+    model: ModelReference
     layer_range: Tuple[int, int]
     parameters: Optional[Dict[str, ParameterSetting]] = None
 
 
 class InputModelDefinition(BaseModel):
-    model: str
+    model: ModelReference
     parameters: Optional[Dict[str, ParameterSetting]] = None
 
 
 class OutputSliceDefinition(BaseModel):
     sources: List[InputSliceDefinition]
-    base_model: Optional[str] = None
+    base_model: Optional[ModelReference] = None
     residual_weight: Optional[float] = None
     parameters: Optional[Dict[str, ParameterSetting]] = None
 
@@ -85,26 +85,22 @@ class MergeConfiguration(BaseModel):
     merge_method: str
     slices: Optional[List[OutputSliceDefinition]] = None
     models: Optional[List[InputModelDefinition]] = None
-    input_model_parameters: Dict[str, Dict[str, ParameterSetting]] = None
     parameters: Optional[Dict[str, ParameterSetting]] = None
-    base_model: Optional[str] = None
+    base_model: Optional[ModelReference] = None
     dtype: Optional[str] = None
     tokenizer_source: Optional[str] = None
 
     def referenced_models(self) -> List[ModelReference]:
         models = set()
         if self.base_model:
-            models.add(ModelReference.parse(self.base_model))
-        if self.input_model_parameters:
-            for key in self.input_model_parameters:
-                models.add(ModelReference.parse(key))
+            models.add(self.base_model)
         if self.models:
             for model_in in self.models:
-                models.add(ModelReference.parse(model_in.model))
+                models.add(model_in.model)
         if self.slices:
             for s in self.slices:
                 for src in s.sources:
-                    models.add(ModelReference.parse(src.model))
+                    models.add(src.model)
         return list(models)
 
     @model_validator(mode="after")
@@ -122,10 +118,9 @@ class MergeConfiguration(BaseModel):
 
 class ConfigReader(BaseModel):
     config: MergeConfiguration
-    tensor_name: str
     t: float
-    slice_out: Optional[OutputSliceDefinition]
-    slices_in: Optional[List[InputSliceDefinition]]
+    tensor_name: Optional[str] = None
+    slice_out: Optional[OutputSliceDefinition] = None
 
     @property
     def base_model(self) -> Optional[ModelReference]:
@@ -134,9 +129,31 @@ class ConfigReader(BaseModel):
         else:
             res = self.config.base_model
 
-        if res:
-            return ModelReference.parse(res)
-        return None
+        return res
+
+    def for_out_slice(self, slice: OutputSliceDefinition) -> "ConfigReader":
+        return ConfigReader(
+            config=self.config,
+            t=self.t,
+            tensor_name=self.tensor_name,
+            slice_out=slice,
+        )
+
+    def for_tensor(self, tensor_name: str) -> "ConfigReader":
+        return ConfigReader(
+            config=self.config,
+            t=self.t,
+            tensor_name=tensor_name,
+            slice_out=self.slice_out,
+        )
+
+    def with_t(self, t: float) -> "ConfigReader":
+        return ConfigReader(
+            config=self.config,
+            t=t,
+            tensor_name=self.tensor_name,
+            slice_out=self.slice_out,
+        )
 
     def parameter(
         self,
@@ -145,33 +162,19 @@ class ConfigReader(BaseModel):
         default: Any = None,
         required: bool = False,
     ) -> Any:
-        if model and self.slices_in:
-            for s in self.slices_in:
-                if s.model == str(model) and s.parameters and name in s.parameters:
-                    value = evaluate_setting(
-                        self.tensor_name, s.parameters[name], self.t
-                    )
-                    if value is not None:
-                        return value
-
         if self.slice_out:
+            if model:
+                for s in self.slice_out.sources:
+                    if s.model == model and s.parameters and name in s.parameters:
+                        value = evaluate_setting(
+                            self.tensor_name, s.parameters[name], self.t
+                        )
+                        if value is not None:
+                            return value
+
             if self.slice_out.parameters and name in self.slice_out.parameters:
                 value = evaluate_setting(
                     self.tensor_name, self.slice_out.parameters[name], self.t
-                )
-                if value is not None:
-                    return value
-
-        if (
-            self.config.input_model_parameters
-            and model
-            and str(model) in self.config.input_model_parameters
-        ):
-            if name in self.config.input_model_parameters[self.slice_in.model]:
-                value = evaluate_setting(
-                    self.tensor_name,
-                    self.config.input_model_parameters[str(model)][name],
-                    self.t,
                 )
                 if value is not None:
                     return value
@@ -186,11 +189,9 @@ class ConfigReader(BaseModel):
                 return value
 
         if required:
-            suffix = (
-                f" for {str(model)}.{self.tensor_name}"
-                if model
-                else f" for {self.tensor_name}"
-            )
+            path_paths = [str(s) for s in [model, self.tensor_name] if s]
+            p = ".".join(path_paths)
+            suffix = f" for {p}" if p else ""
             raise RuntimeError(f"Missing required parameter {name}{suffix}")
         return default
 

mergekit/io/__init__.py
@@ -20,4 +20,9 @@ from mergekit.io.lazy_tensor_loader import (
 )
 from mergekit.io.tensor_writer import TensorWriter
 
-__all__ = ["LazyTensorLoader", "ShardedTensorIndex", "ShardInfo", "TensorWriter"]
+__all__ = [
+    "LazyTensorLoader",
+    "ShardedTensorIndex",
+    "ShardInfo",
+    "TensorWriter",
+]

commit 7d380be8efa05dd16b1151496f831d9a122896c3

Fix passthrough scale (#189)
Passthrough `scale` parameter was broken by #127. Fix it.

mergekit/merge_methods/passthrough.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see http://www.gnu.org/licenses/.
 
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List
 
 import torch
 from torch._tensor import Tensor
@@ -26,7 +26,7 @@ from mergekit.merge_methods.base import ConfigParameterDef, MergeMethod
 
 class PassthroughMergeTask(Task[torch.Tensor]):
     gather_tensors: GatherTensors
-    scale: Optional[float] = None
+    tensor_parameters: ImmutableMap[ModelReference, ImmutableMap[str, Any]]
 
     def arguments(self) -> Dict[str, Task]:
         return {"tensors": self.gather_tensors}
@@ -35,22 +35,25 @@ class PassthroughMergeTask(Task[torch.Tensor]):
         if len(tensors) != 1:
             raise RuntimeError("Passthrough merge expects exactly one tensor")
 
-        res = list(tensors.values())[0]
-        if self.scale is not None:
-            res *= self.scale
+        model, tensor = list(tensors.items())[0]
+        scale = self.tensor_parameters[model].data.get("scale", None)
+        if scale is not None:
+            tensor *= scale
 
-        return res
+        return tensor
 
 
 class PassthroughMerge(MergeMethod):
-    def parameters(self) -> List[ConfigParameterDef]:
+    def tensor_parameters(self) -> List[ConfigParameterDef]:
         return [ConfigParameterDef(name="scale", required=False, default_value=None)]
 
     def make_task(
         self,
         *,
         tensors: GatherTensors,
-        parameters: ImmutableMap[str, Any],
+        tensor_parameters: ImmutableMap[ModelReference, ImmutableMap[str, Any]],
         **kwargs,
     ) -> Task:
-        return PassthroughMergeTask(gather_tensors=tensors, scale=parameters["scale"])
+        return PassthroughMergeTask(
+            gather_tensors=tensors, tensor_parameters=tensor_parameters
+        )

tests/test_basic_merges.py
@@ -11,6 +11,7 @@ from mergekit.config import (
     OutputSliceDefinition,
     ParameterSetting,
 )
+from mergekit.io import LazyTensorLoader, ShardedTensorIndex
 
 
 @pytest.fixture(scope="session")
@@ -54,6 +55,38 @@ class TestBasicMerges:
             assert config.n_layer == 24
 
         run_and_check_merge(config, validate=_check_config_layers)
+
+    def test_passthrough_scale(self, model_a):
+        config = MergeConfiguration(
+            merge_method="passthrough",
+            models=[
+                InputModelDefinition(
+                    model=model_a,
+                    parameters={
+                        "scale": [
+                            {"filter": "o_proj", "value": 0},
+                            {"value": 1},
+                        ]
+                    },
+                )
+            ],
+        )
+
+        def _check_o_proj(p: str):
+            loader = LazyTensorLoader(index=ShardedTensorIndex.from_disk(p))
+            saw_any = False
+            for name in loader.index.tensor_paths:
+                if "o_proj" in name:
+                    param = loader.get_tensor(name)
+                    assert (param == 0).all()
+                    saw_any = True
+                elif "lm_head" in name:
+                    param = loader.get_tensor(name)
+                    assert param.count_nonzero() > 0
+
+            assert saw_any, "No o_proj parameters found"
+
+        run_and_check_merge(config, validate=_check_o_proj)
 
     def test_linear_merge(self, model_a, model_b):
         config = self.two_model_config(model_a, model_b, merge_method="linear")