912760de73c9d28b6669569fb5a39b3f7d78a544
nthangelane <nkululekoc1@gmail.com>
2024-09-23 14:42:20 +0200

    comments on changes

--- mergekit/scripts/ga_evolve.py
+++ mergekit/scripts/ga_evolve.py
@@ -12,6 +12,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see http://www.gnu.org/licenses/.
+#Nkululeko Thangelane Ga changes
 
 import logging
 import os
